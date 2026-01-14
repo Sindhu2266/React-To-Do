67328bcda2055f13344788d6a26c75c8b36dd0fd
Sindhu2266 <sindhu5631@gmail.com>
2026-01-14 13:25:22 +0530

    Updated UI, added icons and improved styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React, { useState } from 'react';
 import "./App.css";
+import todoIcon from './Assets/list-icon.png';
 
 
+const getRandomLightColor = () => {
+  const r = Math.floor(Math.random() * 156 + 100);
+  const g = Math.floor(Math.random() * 156 + 100);
+  const b = Math.floor(Math.random() * 156 + 100);
+  return `rgb(${r}, ${g}, ${b})`;
+};
 const App=()=> {
   const [todo,settodo] = useState("");
   const [todos, settodos] = useState([]);
@@ -12,20 +19,31 @@ const handlesubmit = (e) =>{
 
  if (editid) {
   const edittodo = todos.find((i) => i.id === editid);
-  const updatedtodos = todos.map((t) => 
-  t.id === edittodo.id ?
-  (t = {id: t.id, todo})
-  :{ id: t.id, todo: t.todo}
+
+  
+
+const updatedtodos = todos.map(t =>
+    t.id === editid ? { ...t, todo } : t
   );
   settodos(updatedtodos);
   setEditid(0);
   settodo("");
   return;
+
+  
  }
 
 
-if(todo !== ''){
-  settodos([{id: `${todo} - ${Date.now()}` , todo},...todos]);
+
+ if (todo !== '') {
+  settodos([
+    {
+      id: `${todo}-${Date.now()}`,
+      todo,
+      color: getRandomLightColor()
+    },
+    ...todos
+  ]);
   settodo("");
 }
 };
@@ -42,23 +60,36 @@ const handleEdit = (id) => {
 };
 
 
+
   return <div className="App">
     <div className="container">
-    <h1>Todo List App</h1>
+    
+    <div className="title">
+  <img src={todoIcon} alt="Todo Icon" className="title-icon" />
+  <h1>Todo List App</h1>
+</div>
     <form className="todoform" onSubmit={handlesubmit}>
       <input type='text' value={todo} onChange={(e) => settodo(e.target.value)}></input>
       <button type="submit">{editid ? "EDIT" : "ADD"}</button>
       </form> 
+
       <ul className="alltodos">
         {todos.map((t) => (
-        <li className="singletodo">
-         <span className="todotext" key={t.id}>
+        <li className="singletodo"
+      key={t.id}
+      style={{ backgroundColor: t.color }}>
+         <span className="todotext" >
           {t.todo}</span>
-         <button onClick={() => handleEdit(t.id)}>Edit</button> 
-         <button onClick={() => handledelete(t.id)}> Delete</button>
+         
+         <div className="actions">
+    <button onClick={() => handleEdit(t.id)}>Edit</button>
+    <button onClick={() => handledelete(t.id)}>Delete</button>
+  </div>
       </li>))}
         
       </ul>
+
+
     </div>
   </div>
 };
